2bcdbb74a402c95f8328aaef18f1f02ba87f154f
Deepak Shreevastava <Dpkshreevastava@gmail.com>
2022-11-15 13:50:49 +0530

    added focus on load

---
+++ script.js
@@ -0,0 +1,20 @@
+// function allnumeric(inputtxt)
+//    {
+//       var numbers = /^[0-9]+$/;
+//       if(inputtxt.value.match(numbers))
+//       {
+//       alert('Your Registration number has accepted....');
+//       document.form1.text1.focus();
+//       return true;
+//       }
+//       else
+//       {
+//       alert('Please input numeric characters only');
+//       document.form1.text1.focus();
+//       return false;
+//       }
+//    } 
+
+let ind = document.getElementById("my-text");
+console.log(ind);
+ind.focus();
